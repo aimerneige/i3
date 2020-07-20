49808f44d5c1d7b5463bae52a695aa98cb9c2f64
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-20 16:02:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# i3 config
+
+## About
+
+My own config for i3.
+
+## System
+
+I am using Ubuntu 20.04
+
+## Useage
+
+**Don't use this config without see what's in the config file!**
+
+```sh
+# First, clone this to your computer
+git clone https://www.github.com/AimerNeige/i3.git
+
+# Read and edit the config file and the autostart script
+vim config
+vim i3Autostart.sh
+
+# Then, move this folder to the ~/.config/
+mv i3/ ~/.config
+
+# Finally, restart the i3 with `Super + Shift + R`
+```
---
+++ i3Autostart.sh
@@ -0,0 +1,27 @@
+# start the shadowsocksr
+# cd /usr/local/share/shadowsocksr/shadowsocks/
+# sudo python local.py -d start
+
+# Setup Workspace 1
+i3-msg "workspace 1; exec st"
+sleep 1;
+
+# Setup Workspace 2
+i3-msg "workspace 2; exec firefox"
+sleep 3;
+
+# Setup Workspace 3
+i3-msg "workspace 3; exec netease-cloud-music"
+sleep 3.5;
+
+# Setup Workspace 4
+i3-msg "workspace 4; exec telegram-desktop"
+sleep 2.5;
+
+# Setup Workspace 5
+i3-msg "workspace 5; exec code"
+sleep 3;
+
+# Setup Workspace 5
+i3-msg "workspace 5; exec alacritty"
+sleep 1;
