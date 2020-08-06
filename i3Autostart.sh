a2b0363dd6f73e52ebc9a1b0011e3fa929118118
AimerNeige <aimer.neige.soft@gmail.com>
2020-08-06 22:33:49 +0800

    none

--- i3Autostart.sh
+++ i3Autostart.sh
@@ -3,25 +3,25 @@
 # sudo python local.py -d start
 
 # Setup Workspace 1
-i3-msg "workspace 1; exec alacritty"
-sleep 1;
+# i3-msg "workspace 1; exec alacritty"
+# sleep 1;
 
 # Setup Workspace 2
-i3-msg "workspace 2; exec google-chrome"
-sleep 3;
+# i3-msg "workspace 2; exec google-chrome"
+# sleep 3;
 
 # Setup Workspace 3
-i3-msg "workspace 3; exec netease-cloud-music"
-sleep 3.5;
+# i3-msg "workspace 3; exec netease-cloud-music"
+# sleep 3.5;
 
 # Setup Workspace 4
-i3-msg "workspace 4; exec telegram-desktop"
-sleep 2.5;
+# i3-msg "workspace 4; exec telegram-desktop"
+# sleep 2.5;
 
 # Setup Workspace 5
-i3-msg "workspace 5; exec code"
-sleep 3;
+# i3-msg "workspace 5; exec code"
+# sleep 3;
 
 # Setup Workspace 6
-i3-msg "workspace 6; exec virtualbox"
-sleep 1;
+# i3-msg "workspace 6; exec virtualbox"
+# sleep 1;
